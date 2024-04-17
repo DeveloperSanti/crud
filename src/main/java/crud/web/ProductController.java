2c7980f7c28b66f7944deb4a2c132009404d16b1
DevSantiago <developer.santi77@gmail.com>
2024-04-17 00:39:28 -0500

    Create class ProductController

---
+++ src/main/java/crud/web/ProductController.java
@@ -0,0 +1,13 @@
+package crud.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/products")
+public class ProductController {
+
+
+
+}
