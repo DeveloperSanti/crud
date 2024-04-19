8bd410dd6194223bbd5ebe996edaabdbb98d40e2
DevSantiago <developer.santi77@gmail.com>
2024-04-19 00:05:52 -0500

    Made petitions save and delete in the Controller of product

--- src/main/java/crud/web/ProductController.java
+++ src/main/java/crud/web/ProductController.java
@@ -18,13 +18,27 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/getAll")
-    public List<ProductEntity> getAll(){
-        return productService.getAll();
+    public ResponseEntity<List<ProductEntity>> getAll(){
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{idProduct}")
     public ResponseEntity<ProductEntity> getProductById(@PathVariable("idProduct") Long idProduct){
         return productService.getProductById(idProduct).map(product -> new ResponseEntity<>(product, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<ProductEntity> save(@RequestBody ProductEntity product){
+        return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{idProduct}")
+    public ResponseEntity delete(@PathVariable("idProduct") Long idProduct){
+        if(productService.delete(idProduct)){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
